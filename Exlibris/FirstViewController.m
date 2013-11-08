7a7ec6c242dda64bfa91be6e2769b89fc23d59b6
xehivs <pawel@ksienie.com>
2013-11-08 22:59:32 +0100

    Initial Commit

---
+++ Exlibris/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  Exlibris
+//
+//  Created by Paweł Ksieniewicz on 08.11.2013.
+//  Copyright (c) 2013 Paweł Ksieniewicz. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Exlibris/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Exlibris
+//
+//  Created by Paweł Ksieniewicz on 08.11.2013.
+//  Copyright (c) 2013 Paweł Ksieniewicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
